8282fc14b8e91ed06c1fc1b000c8075b1e49865c
曹学亮 <2357747972@qq.com>
2018-01-09 15:23:26 +0800

    备份

--- EffectiveObjective-C2.0/EffectiveObjective-C2.0/ViewController.m
+++ EffectiveObjective-C2.0/EffectiveObjective-C2.0/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong) UIButton *tapButton;
 @end
 
 @implementation ViewController
@@ -19,6 +19,22 @@
     self.navigationItem.title = @"首页";
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
     
+    _tapButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_tapButton setTitle:@"点击" forState:UIControlStateNormal];
+    [_tapButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [_tapButton addTarget:self action:@selector(showuAlert) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_tapButton];
+    _tapButton.frame = CGRectMake(0, 0, 100, 30);
+    _tapButton.center = self.view.center;
+}
+
+- (void)showuAlert{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"标题" message:@"" preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alert addAction:action];
+    [self.navigationController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
